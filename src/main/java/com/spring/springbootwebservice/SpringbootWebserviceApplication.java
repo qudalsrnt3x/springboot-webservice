3bff7c7dbd45a634652a0c0d84fbb28fa73d59b9
qudals <qudalsrnt3x@gmail.com>
2021-02-12 02:01:30 +0900

    Add HelloController

--- src/main/java/com/spring/springbootwebservice/SpringbootWebserviceApplication.java
+++ src/main/java/com/spring/springbootwebservice/SpringbootWebserviceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 스프링의 자동 설정, 스프링 Bean 읽기, 생성을 자동으로 설정해준다.
+// 항상 프로젝트 최상단에 위치해야 한다. (componentScan)
 public class SpringbootWebserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebserviceApplication.class, args);
     }
+    // 내장 WAS 를 사용하는 이유 : 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있다.
 
 }
