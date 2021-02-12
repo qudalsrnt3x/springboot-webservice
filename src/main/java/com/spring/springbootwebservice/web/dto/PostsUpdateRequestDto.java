66a872593f206c009f0d0fffc26c6bfe832d4e08
qudalsrnt3x <qudalsrnt3x@gmail.com>
2021-02-13 00:03:43 +0900

    Update PostsApiControllerTest - API Update

--- src/main/java/com/spring/springbootwebservice/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/com/spring/springbootwebservice/web/dto/PostsUpdateRequestDto.java
@@ -2,8 +2,10 @@ package com.spring.springbootwebservice.web.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PostsUpdateRequestDto {
 
     private String title;
